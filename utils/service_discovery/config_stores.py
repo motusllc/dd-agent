1f09ba0cc855d2e0e3b36885697100b19395f5d0
Scott Rankin <srankin@motus.com>
2017-03-14 15:37:18 -0400

    Adding support for JSON service discovery config

--- utils/service_discovery/config_stores.py
+++ utils/service_discovery/config_stores.py
@@ -8,9 +8,10 @@ from utils.service_discovery.abstract_config_store import CONFIG_FROM_AUTOCONF, 
 
 from utils.service_discovery.etcd_config_store import EtcdStore
 from utils.service_discovery.consul_config_store import ConsulStore
+from utils.service_discovery.json_file_config_store import JsonStore
 
 
-SD_CONFIG_BACKENDS = ['etcd', 'consul']  # noqa: used somewhere else
+SD_CONFIG_BACKENDS = ['etcd', 'consul', 'json']  # noqa: used somewhere else
 SD_TEMPLATE_DIR = '/datadog/check_configs'
 
 
@@ -19,6 +20,8 @@ def get_config_store(agentConfig):
         return EtcdStore(agentConfig)
     elif agentConfig.get('sd_config_backend') == 'consul':
         return ConsulStore(agentConfig)
+    elif agentConfig.get('sd_config_backend') == 'json':
+        return JsonStore(agentConfig)
     else:
         return StubStore(agentConfig)
 
